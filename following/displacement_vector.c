d535094085da8976ae0286876e9f0f8cf5fa5006
biosy <aktor.alexis@hotmail.fr>
2017-10-10 13:45:55 +0200

    compute euclidian distance between matching points

--- following/displacement_vector.c
+++ following/displacement_vector.c
@@ -1,10 +1,10 @@
 #include "displacement_vector.h"
 
-int cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
+double cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
 
 	int i;
 	int j;
-	int sum=0;
+	double sum=0;
 	int x;
 	int y;
 
@@ -25,24 +25,24 @@ int cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, 
   * distance euclidienne
 */
 
-int euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
+double euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
 
         int i;
         int j;
-        int sum=0;
+        double sum=0;
         int x;
         int y;
 
         //cross correlation
         for(j=-size_win_y/2;j< size_win_y/2;j++){
                 for(i=-size_win_x/2;i< size_win_x /2;i++){
-                        sum = sum + sqrt(pow(image_1[ptx1 + i][pty1 + j],2) - pow(image_2[ptx2 + i][pty2 + j],2));
+                        sum = sum + pow(image_2[ptx1 + i][pty1 + j] - image_1[ptx2 + i][pty2 + j],2);
                 }
         }
 
 
 
-        return sum;
+        return sqrt(sum);
 
 }
 
@@ -56,12 +56,23 @@ int matching_point(uchar** image_1 , uchar** image_2,int size_x, int size_y, int
 
 	int i;
 	int j;
+	double distance=0;
+	double distance_prev= 100000000;
+
+	int ptx_nearest;
+	int pty_nearest;
 
 	for(j=pty - window_size /2; i< pty + window_size /2; j++){
 		for(i= ptx - window_size /2; i< ptx + window_size /2; i++){
-			if((j > 0) && (j< size_y)&&(i> 0)&&(i < size_x)){
+			if((j > 0) && (j< size_y)&&(i> 0)&&(i < size_x) && (harris2[i][j] == 255)){
 				// method 1 : choose the minimum euclidian distance between points
-				
+				distance = sqrt(pow(i - ptx, 2) + pow(j- pty, 2));
+				if(distance < distance_prev){
+					distance_prev = distance;
+					ptx_nearest = i;
+					pty_nearest = j;
+				}
+
 				// method 2 : choose the minimum euclidian distance between patch
 
 			}
--- following/displacement_vector.h
+++ following/displacement_vector.h
@@ -9,9 +9,9 @@
 #include <cv.h>
 #include <pthread.h>
 
-int cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
+double cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
 int matching_point(uchar** image_1 , uchar** image_2, int size_x, int size_y, int ptx, int pty, uchar** harris2, int window_size);
-int euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
+double euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
 
 
 
