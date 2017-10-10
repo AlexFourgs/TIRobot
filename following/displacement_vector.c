8e520773ba11513b617f778b10c679b3eef13d7f
biosy <aktor.alexis@hotmail.fr>
2017-10-10 10:25:11 +0200

    add almost all the cross correlation

--- following/displacement_vector.c
+++ following/displacement_vector.c
@@ -1,17 +1,17 @@
 #include "displacement_vector.h"
 
-int** sum_square_distance(int** image_1, int** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
+int** cross_correlation(int** image_1, int** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
 
 	int i;
 	int j;
-	int sum;
+	int sum=0;
 	int x;
 	int y;
 
-	//browse window 1 and 2
-	for(j=size_y /2 - size_win_y;j< size_y /2 + size_win_y;j++){
-		for(i=size_x /2 - size_win_x;i< size_x /2 + size_win_x;i++){
-			
+	//cross correlation
+	for(j=-size_win_y;j< size_win_y;j++){
+		for(i=-size_win_x;i< size_win_x;i++){
+			sum = sum + image_1[ptx1 + i][pty1 + j] * image_2[ptx2 + i][pty2 + j];
 		}
 	}
 
--- following/displacement_vector.h
+++ following/displacement_vector.h
@@ -9,7 +9,7 @@
 #include <cv.h>
 #include <pthread.h>
 
-int** sum_square_distance(int** image_1, int** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
+int** cross_correlation(int** image_1, int** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
 
 
 
