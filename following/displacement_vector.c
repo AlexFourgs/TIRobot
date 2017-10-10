9cb03ee1868bf3aecae5a850760deaf4f1172faf
biosy <aktor.alexis@hotmail.fr>
2017-10-10 13:27:46 +0200

    improve matching algorithm with a specfic searching patch

--- following/displacement_vector.c
+++ following/displacement_vector.c
@@ -21,6 +21,10 @@ int cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, 
 
 }
 
+/**
+  * distance euclidienne
+*/
+
 int euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
 
         int i;
@@ -42,16 +46,41 @@ int euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y,
 
 }
 
+/**
+  * match one point of image 1 with all corners of image 2
+*/
 
-int matching_point(uchar** image_1 , uchar** image_2, int ptx, int pty, uchar** harris2, int window_size){
+int matching_point(uchar** image_1 , uchar** image_2,int size_x, int size_y, int ptx, int pty, uchar** harris2, int window_size){
 
 	//browse all the harris points from the image 1 and compare with all harris point of image2
 
 	int i;
 	int j;
 
-	
-	//compare them with cross correlation
+	for(j=pty - window_size /2; i< pty + window_size /2; j++){
+		for(i= ptx - window_size /2; i< ptx + window_size /2; i++){
+			if((j > 0) && (j< size_y)&&(i> 0)&&(i < size_x)){
+				// method 1 : choose the minimum euclidian distance between points
+				
+				// method 2 : choose the minimum euclidian distance between patch
+
+			}
+		}
+	}
+
 	//keep the minimum value
 	return 0;
 }
+
+/*int main() {
+	uchar img1[3][3];
+	img1[0][0]=0;
+	img1[0][1]=255;
+	img1[0][2]=0;
+	img1[1][0]=255;
+	img1[1][1]=255;
+	img1[1][2]=0;
+	img1[2][0]=255;
+	img1[2][1]=0;
+	img1[2][2]=255;
+}*/
--- following/displacement_vector.h
+++ following/displacement_vector.h
@@ -10,7 +10,7 @@
 #include <pthread.h>
 
 int cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
-int matching_point(uchar** image_1 , uchar** image_2, int ptx, int pty, uchar** harris2, int window_size);
+int matching_point(uchar** image_1 , uchar** image_2, int size_x, int size_y, int ptx, int pty, uchar** harris2, int window_size);
 int euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y);
 
 
