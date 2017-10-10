396be2ed33143cd247f96f04d036a4265fe000b1
biosy <aktor.alexis@hotmail.fr>
2017-10-10 13:03:59 +0200

    start of the algorithm of matching

--- following/displacement_vector.c
+++ following/displacement_vector.c
@@ -1,6 +1,6 @@
 #include "displacement_vector.h"
 
-int cross_correlation(int** image_1, int** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
+int cross_correlation(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
 
 	int i;
 	int j;
@@ -21,8 +21,37 @@ int cross_correlation(int** image_1, int** image_2, int size_x, int size_y, int 
 
 }
 
-int matching_point(int ** image_1, int** image_2, uchar** harris1, uchar** harris2){
+int euclidian_distance(uchar** image_1, uchar** image_2, int size_x, int size_y, int ptx1, int ptx2, int pty1, int pty2, int size_win_x, int size_win_y){
+
+        int i;
+        int j;
+        int sum=0;
+        int x;
+        int y;
+
+        //cross correlation
+        for(j=-size_win_y/2;j< size_win_y/2;j++){
+                for(i=-size_win_x/2;i< size_win_x /2;i++){
+                        sum = sum + sqrt(pow(image_1[ptx1 + i][pty1 + j],2) - pow(image_2[ptx2 + i][pty2 + j],2));
+                }
+        }
 
 
+
+        return sum;
+
+}
+
+
+int matching_point(uchar** image_1 , uchar** image_2, int ptx, int pty, uchar** harris2, int window_size){
+
+	//browse all the harris points from the image 1 and compare with all harris point of image2
+
+	int i;
+	int j;
+
+	
+	//compare them with cross correlation
+	//keep the minimum value
 	return 0;
 }
